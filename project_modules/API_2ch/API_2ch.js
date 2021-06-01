ade4efea31c63293bd4fe73b5b72fcd8b15591f3
vertically-challenged <50969768+vertically-challenged@users.noreply.github.com>
2021-06-01 12:32:32 +0300

    Добавлены проверки 404 ошибки в fetch()

--- project_modules/API_2ch/API_2ch.js
+++ project_modules/API_2ch/API_2ch.js
@@ -7,7 +7,17 @@ class API_2ch {
     // Принимает букву доски и возвращает JSON страницы этой доски
     static async getListOfBoardThreads (boardLetter = config.DEFAULT_BOARD) {
         try {
-            let allThreads = await fetch(HelperForStr.substringReplacement(config.ALL_THREADS_LINK, config.PLUGS.BOARD, boardLetter)).then(res => res.json())
+            let allThreads = await fetch(HelperForStr.substringReplacement(config.ALL_THREADS_LINK, config.PLUGS.BOARD, boardLetter))
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(
+                    `Failed with HTTP code ${res.status}
+                     boardLetter: /${boardLetter}/
+                    `);
+                }
+                return res;
+            })
+            .then(res => res.json())
 
             return allThreads
         } catch (err) {
@@ -18,7 +28,18 @@ class API_2ch {
     // Принимает букву доски и номер первого поста треда, возвращает JSON страницы треда
     static async getThread (boardLetter = config.DEFAULT_BOARD, threadNumber = 0) {
         try {
-            let thread = await fetch(HelperForStr.substringReplacement(HelperForStr.substringReplacement(config.THREAD_LINK, config.PLUGS.BOARD, boardLetter), config.PLUGS.THREAD_NUMBER, threadNumber)).then(res => res.json())
+            let thread = await fetch(HelperForStr.substringReplacement(HelperForStr.substringReplacement(config.THREAD_LINK, config.PLUGS.BOARD, boardLetter), config.PLUGS.THREAD_NUMBER, threadNumber))
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(
+                    `Failed with HTTP code ${res.status}
+                     boardLetter: /${boardLetter}/
+                     threadNumber: #${threadNumber}
+                    `);
+                }
+                return res;
+            })
+            .then(res => res.json())
         
             return thread
         } catch (err) {
@@ -31,9 +52,10 @@ class API_2ch {
         let arrayOfPosts = []
         try {
             let thread = await API_2ch.getThread(boardLetter, threadNumber)
-            for (let post of thread.threads[0].posts) {
-                console.log(post)
-                arrayOfPosts.push(post)
+            if (thread) {
+                for (let post of thread.threads[0].posts) {
+                    arrayOfPosts.push(post)
+                }
             }
         } catch (err) {
             console.log(err)
